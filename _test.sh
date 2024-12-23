010e967fbc690b536fa57784025b46045e21f1d9
manar <manar.zaboub@gmail.com>
2024-12-23 14:24:10 +0100

    Automated commit via Ansible

--- _test.sh
+++
@@ -1,4 +0,0 @@
-echo $(. /etc/os-release && echo "$VERSION_CODENAME")"" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-  echo "$(. /etc/os-release && [ $VERSION_CODENAME = 'wilma' ] && echo "Linux mint detected changing to noble" && echo 'noble' || echo "$VERSION_CODENAME")"
